ff9fb11010a02a58b5cdacb5a401a0b445be1651
Alvin Kiveu <67068458+alvin-kiveu@users.noreply.github.com>
2023-11-19 20:06:49 +0300

    adding new data

--- configs.php
+++ configs.php
@@ -1,3 +1,3 @@
 <?php
-$SecretKey = "sk_live_71089f16a3aa3dbfeff53aeca3721aacebfe5907"; // Add your secret key here. Remember to change this to your live secret key in production
-$PublicKey = "pk_live_05aec9c33ed4eb4344f5b9fc5c964b3b81808484"; // Add your public key here. Remember to change this to your live public key in production
+$SecretKey = ""; // Add your secret key here. Remember to change this to your live secret key in production
+$PublicKey = ""; // Add your public key here. Remember to change this to your live public key in production
